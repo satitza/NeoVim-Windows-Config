6de525c0b9084f1548181545e4b9c1839fef8a38
Satit Porntepanon <st_satitza@hotmail.com>
2025-08-29 01:54:54 +0700

    add debug config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,13 +3,6 @@ return {
     "theHamsta/nvim-dap-virtual-text",
     "nvim-neotest/nvim-nio",
     "williamboman/mason.nvim",
-
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-
-      dapui.setup()
-    end,
   },
 
   {
@@ -30,8 +23,83 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
+
+      -- 🔹 Python debug adapter
+      dap.adapters.python = {
+        type = "executable",
+        command = "python", -- ชี้ไปที่ python ที่มี debugpy
+        args = { "-m", "debugpy.adapter" },
+      }
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            return "python"
+          end,
+        },
+      }
+
+      -- 🔹 C / C++ debug adapter (ผ่าน cpptools)
+      dap.adapters.cppdbg = {
+        id = "cppdbg",
+        type = "executable",
+        command = "C:\\Users\\st_sa\\.vscode\\extensions\\ms-vscode.cpptools-1.27.2-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe", -- มาจาก cpptools
+      }
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "cppdbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtEntry = true,
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+
+      -- 🔹 JavaScript / TypeScript (ผ่าน node2 adapter)
+      dap.adapters.node2 = {
+        type = "executable",
+        command = "node",
+        args = { vim.fn.stdpath("data") .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
+      }
+      dap.configurations.javascript = {
+        {
+          name = "Launch file",
+          type = "node2",
+          request = "launch",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+        },
+      }
+
+      -- 🔹 C# / .NET (OmniSharp)
+      dap.adapters.coreclr = {
+        type = "executable",
+        command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+        args = { "--interpreter=vscode" },
+      }
+      dap.configurations.cs = {
+        {
+          type = "coreclr",
+          name = "Launch - netcoredbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          end,
+        },
+      }
     end,
   },
+
+  -- 🔹 Go debug
   {
     "leoluz/nvim-dap-go",
     dependencies = { "mfussenegger/nvim-dap" },
